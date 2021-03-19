3abd102232d0541a66c1a934154fe290fdebf334
JanPfenning <jan.pfenning@gmail.com>
2021-03-19 23:13:34 +0100

    visual refactor

--- src/app/lobby/lobby.module.ts
+++ src/app/lobby/lobby.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LobbyComponent } from './lobby/lobby.component';
 import {SharedModule} from '../shared/shared.module';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [LobbyComponent],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        MatCardModule
+    ]
 })
 export class LobbyModule { }
